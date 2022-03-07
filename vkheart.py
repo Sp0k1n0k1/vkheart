870d98241f7be516262db4376480b5d4c029febd
Sp0k1n0k1 <96115844+Sp0k1n0k1@users.noreply.github.com>
2022-03-08 01:15:25 +0900

    Update vkheart.py

--- vkheart.py
+++ vkheart.py
@@ -1,10 +1,11 @@
 #By Volkov Alexey | https://vk.com/sp0ki_n0ki
 #Heart Project
-#0. Добываем access_token
-#0.1 Вписываем в token= Твой токен
-#1. https://dev.vk.com/method/messages.send веддите  напишите айди собеседника, удалите из строки peer_id ВСЕ а в message введите что хотите!
-#2. Копируем респонс числа
-#3. Заменяем в 0,коде message_id=число из респонс
+# Инструкция
+#0. Добываем access_token (https://vkhost.github.io)
+#0.1 Вписываем в access_token=токен который ты добыл
+#1. https://dev.vk.com/method/messages.send 1. Вводим наш токен (AccessToken) 2. Вводим в peer_id айди собеседника (УДАЛИТЕ ЧУЖОЙ АЙДИ) 3. Нажимаем выполнить
+#2. Копируем респонс числа (В результате там будет)
+#3. CTRL-H Находим id и заменяем на айди собеседника, а messageid заменяем на число из результата response (Респонс)
 #4. Запускаем
 
 import vk_api
